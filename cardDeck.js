49d8f778491b015fd1bbd99c320b974c21c5727c
Kyle Larson <113455306+aaaknot30@users.noreply.github.com>
2022-10-17 16:51:30 -0600

    Add files via upload

---
+++ cardDeck.js
@@ -0,0 +1,263 @@
+let cardDeck = [
+    {
+        "name": "A_spades",
+        "image": "./images/cards/A_spades.png",
+        "value": 11
+    },
+    {
+        "name": "2_spades",
+        "image": "./images/cards/2_spades.png",
+        "value": 2
+    },
+    {
+        "name": "3_spades",
+        "image": "./images/cards/3_spades.png",
+        "value": 3
+    },
+    {
+        "name": "4_spades",
+        "image": "./images/cards/4_spades.png",
+        "value": 4
+    },
+    {
+        "name": "5_spades",
+        "image": "./images/cards/5_spades.png",
+        "value": 5
+    },
+    {
+        "name": "6_spades",
+        "image": "./images/cards/6_spades.png",
+        "value": 6
+    },
+    {
+        "name": "7_spades",
+        "image": "./images/cards/7_spades.png",
+        "value": 7
+    },
+    {
+        "name": "8_spades",
+        "image": "./images/cards/8_spades.png",
+        "value": 8
+    },
+    {
+        "name": "9_spades",
+        "image": "./images/cards/9_spades.png",
+        "value": 9
+    },
+    {
+        "name": "10_spades",
+        "image": "./images/cards/10_spades.png",
+        "value": 10
+    },
+    {
+        "name": "J_spades",
+        "image": "./images/cards/J_spades.png",
+        "value": 10
+    },
+    {
+        "name": "Q_spades",
+        "image": "./images/cards/Q_spades.png",
+        "value": 10
+    },
+    {
+        "name": "K_spades",
+        "image": "./images/cards/K_spades.png",
+        "value": 10
+    },
+    {
+        "name": "A_clubs",
+        "image": "./images/cards/A_clubs.png",
+        "value": 11
+    },
+    {
+        "name": "2_clubs",
+        "image": "./images/cards/2_clubs.png",
+        "value": 2
+    },
+    {
+        "name": "3_clubs",
+        "image": "./images/cards/3_clubs.png",
+        "value": 3
+    },
+    {
+        "name": "4_clubs",
+        "image": "./images/cards/4_clubs.png",
+        "value": 4
+    },
+    {
+        "name": "5_clubs",
+        "image": "./images/cards/5_clubs.png",
+        "value": 5
+    },
+    {
+        "name": "6_clubs",
+        "image": "./images/cards/6_clubs.png",
+        "value": 6
+    },
+    {
+        "name": "7_clubs",
+        "image": "./images/cards/7_clubs.png",
+        "value": 7
+    },
+    {
+        "name": "8_clubs",
+        "image": "./images/cards/8_clubs.png",
+        "value": 8
+    },
+    {
+        "name": "9_clubs",
+        "image": "./images/cards/9_clubs.png",
+        "value": 9
+    },
+    {
+        "name": "10_clubs",
+        "image": "./images/cards/10_clubs.png",
+        "value": 10
+    },
+    {
+        "name": "J_clubs",
+        "image": "./images/cards/J_clubs.png",
+        "value": 10
+    },
+    {
+        "name": "Q_clubs",
+        "image": "./images/cards/Q_clubs.png",
+        "value": 10
+    },
+    {
+        "name": "K_clubs",
+        "image": "./images/cards/K_clubs.png",
+        "value": 10
+    },
+    {
+        "name": "A_hearts",
+        "image": "./images/cards/A_hearts.png",
+        "value": 11
+    },
+    {
+        "name": "2_hearts",
+        "image": "./images/cards/2_hearts.png",
+        "value": 2
+    },
+    {
+        "name": "3_hearts",
+        "image": "./images/cards/3_hearts.png",
+        "value": 3
+    },
+    {
+        "name": "4_hearts",
+        "image": "./images/cards/4_hearts.png",
+        "value": 4
+    },
+    {
+        "name": "5_hearts",
+        "image": "./images/cards/5_hearts.png",
+        "value": 5
+    },
+    {
+        "name": "6_hearts",
+        "image": "./images/cards/6_hearts.png",
+        "value": 6
+    },
+    {
+        "name": "7_hearts",
+        "image": "./images/cards/7_hearts.png",
+        "value": 7
+    },
+    {
+        "name": "8_hearts",
+        "image": "./images/cards/8_hearts.png",
+        "value": 8
+    },
+    {
+        "name": "9_hearts",
+        "image": "./images/cards/9_hearts.png",
+        "value": 9
+    },
+    {
+        "name": "10_hearts",
+        "image": "./images/cards/10_hearts.png",
+        "value": 10
+    },
+    {
+        "name": "J_hearts",
+        "image": "./images/cards/J_hearts.png",
+        "value": 10
+    },
+    {
+        "name": "Q_hearts",
+        "image": "./images/cards/Q_hearts.png",
+        "value": 10
+    },
+    {
+        "name": "K_hearts",
+        "image": "./images/cards/K_hearts.png",
+        "value": 10
+    },{
+        "name": "A_diamonds",
+        "image": "./images/cards/A_diamonds.png",
+        "value": 11
+    },
+    {
+        "name": "2_diamonds",
+        "image": "./images/cards/2_diamonds.png",
+        "value": 2
+    },
+    {
+        "name": "3_diamonds",
+        "image": "./images/cards/3_diamonds.png",
+        "value": 3
+    },
+    {
+        "name": "4_diamonds",
+        "image": "./images/cards/4_diamonds.png",
+        "value": 4
+    },
+    {
+        "name": "5_diamonds",
+        "image": "./images/cards/5_diamonds.png",
+        "value": 5
+    },
+    {
+        "name": "6_diamonds",
+        "image": "./images/cards/6_diamonds.png",
+        "value": 6
+    },
+    {
+        "name": "7_diamonds",
+        "image": "./images/cards/7_diamonds.png",
+        "value": 7
+    },
+    {
+        "name": "8_diamonds",
+        "image": "./images/cards/8_diamonds.png",
+        "value": 8
+    },
+    {
+        "name": "9_diamonds",
+        "image": "./images/cards/9_diamonds.png",
+        "value": 9
+    },
+    {
+        "name": "10_diamonds",
+        "image": "./images/cards/10_diamonds.png",
+        "value": 10
+    },
+    {
+        "name": "J_diamonds",
+        "image": "./images/cards/J_diamonds.png",
+        "value": 10
+    },
+    {
+        "name": "Q_diamonds",
+        "image": "./images/cards/Q_diamonds.png",
+        "value": 10
+    },
+    {
+        "name": "K_diamonds",
+        "image": "./images/cards/K_diamonds.png",
+        "value": 10
+    }
+]
+
+export default cardDeck
